377c30613c6543ef31566e00d5156cc882879179
ankit <sahuankit.murhu@gmail.com>
2026-04-23 23:20:35 +0530

    feat: implemented token bucket algorithm

--- src/algo/token_bucket.rs
+++ src/algo/token_bucket.rs
@@ -1,9 +1,60 @@
+/*!
+# Token Bucket Algorithm
+- Bucket has fixed capacity (`bucket_size`)
+- Bucket will follow lazy refill strategy (refill only when a request is made)
+- Tokens are refilled at a constant rate (`refill_rate/second`)
+- Each request consumes 1 token
+- If no tokens are available → request is denied
+- Supports burst traffic up to bucket capacity
+
+Reference: [wiki](https://en.wikipedia.org/wiki/Token_bucket)
+*/
+
+use std::time::Instant;
+
+use crate::ratelimiter::RateLimiter;
+
 pub struct TokenBucket {
-    capacity: usize,
+    bucket_size: usize,
+    refill_rate: f64,
     tokens: usize,
-    refill_rate: usize
+    last_refill_time: Instant,
 }
 
-pub fn run() {
-    println!("Token Bucket Rate Limiter");
+impl TokenBucket {
+    pub fn new(bucket_size: usize, refill_rate: f64) -> Self {
+        Self {
+            bucket_size,
+            refill_rate,
+            tokens: bucket_size,
+            last_refill_time: Instant::now(),
+        }
+    }
+
+    pub fn consume(&mut self) -> bool {
+        self.refill();
+
+        if self.tokens > 0 {
+            self.tokens -= 1;
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn refill(&mut self) {
+        let now = Instant::now();
+        let elapsed = now.duration_since(self.last_refill_time).as_secs_f64();
+
+        let new_tokens = elapsed * self.refill_rate;
+        self.tokens = (new_tokens + self.tokens as f64).min(self.bucket_size as f64) as usize;
+
+        self.last_refill_time = now;
+    }
+}
+
+impl RateLimiter for TokenBucket {
+    fn is_allowed(&mut self) -> bool {
+        self.consume()
+    }
 }

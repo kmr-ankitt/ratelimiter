25eddfd6614badb973ee7b176a5e32a2d71a4343
ankit <sahuankit.murhu@gmail.com>
2026-04-23 21:53:10 +0530

    feat: artix server created

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{HttpResponse, HttpServer, Responder, get};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello, weaks!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Server running at http://localhost:8000");
+
+    HttpServer::new(|| actix_web::App::new().service(index))
+        .bind(("127.0.0.1", 8000))?
+        .run()
+        .await
 }

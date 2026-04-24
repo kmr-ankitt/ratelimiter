3c351b7e03f205062703507000d879c758c41886
ankit <sahuankit.murhu@gmail.com>
2026-04-24 22:52:20 +0530

    docs: fixed window docs added

--- src/algo/fixed_window.rs
+++ src/algo/fixed_window.rs
@@ -1,3 +1,12 @@
+/*! 
+# Fixed Window Rate Limiter 
+
+- Fixed window is a time based window where requests are counted in fixed time intervals
+- It maintains a counter for each window and increments it when a request is made
+- When the window expires, the counter is reset to 0 and the next window starts
+- It is a simple and efficient algorithm, but it has a drawback that it can lead to burst traffic
+*/
+
 use std::time::{Duration, Instant};
 
 use crate::ratelimiter::RateLimiter;
